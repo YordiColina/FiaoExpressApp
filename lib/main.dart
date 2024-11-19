6bde4cb662ed7cf40c209466c52d9e0c7a4557db
Yordis Colina <yordi.colina@untoquedigital.com>
2024-11-19 16:06:59 -0500

    add pre home screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:login/home_screens/home_screen.dart';
 import 'package:login/login_sreens/login_screen.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- login/lib/login_sreens/pre_home.dart
+++ login/lib/login_sreens/pre_home.dart
@@ -11,7 +11,7 @@ class _PreHomeState extends State<PreHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body: Container(),
     );
   }
 }
